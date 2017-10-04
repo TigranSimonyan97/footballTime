2f926fa94c0b837b697ec0e3c1c87cd1d97d4274
Tke <tigsim97@gmail.com>
2017-10-04 21:53:17 +0400

    Added Tab Bar and TableView.Also UIRoundedImageView Class

---
+++ FootballTime/TeamsCell.swift
@@ -0,0 +1,24 @@
+//
+//  TeamsCell.swift
+//  FootballTime
+//
+//  Created by Tiko on 10/4/17.
+//  Copyright © 2017 Tigranakert. All rights reserved.
+//
+
+import UIKit
+
+class TeamsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FootballTime/UIRoundedImageView.swift
@@ -0,0 +1,21 @@
+//
+//  UIRoundedImageView.swift
+//  FootballTime
+//
+//  Created by Tiko on 10/4/17.
+//  Copyright © 2017 Tigranakert. All rights reserved.
+//
+
+import UIKit
+
+class UIRoundedImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
